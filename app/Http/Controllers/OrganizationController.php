8dbef15585cf6d689856b7ad02ded5a4526b59f3
Thierry M <dev@perif.co>
2019-10-29 14:42:28 +0100

    renaming OrganizationController to CompanyController

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Company;
 use Illuminate\Http\Request;
 
-class OrganizationController extends Controller
+class CompanyController extends Controller
 {
     /**
      * Display a listing of the resource.

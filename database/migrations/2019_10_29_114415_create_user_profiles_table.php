26e8fd4bc0fb0339bf5cb72cebd3a408ad527a69
Thierry M <dev@perif.co>
2019-10-29 13:20:53 +0100

    User Profile Class seems to make more sense as it's cleaner and closer to SOLID. Updated the Migration Files also to reflect new classes

---
+++ database/migrations/2019_10_29_114415_create_user_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->string('profile_name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('country');
+            $table->integer('postcode');
+            $table->string('phone');
+            $table->string('linkedin_url');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
